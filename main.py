41ffd6339b636e0674ad9a4ea3743de7f9edf465
ananth-0408 <88501485+ananth-0408@users.noreply.github.com>
2021-10-27 19:23:06 +0530

    added pistols(1)

--- main.py
+++ main.py
@@ -198,6 +198,18 @@ async def on_message(message) :
 
   if '$loadout na' in message.content.lower() :
     await message.reply('NOOOOOOOOOOOOOOOOOOOO :rage:')
+    
+  if '$loadout mw' in message.content.lower() :
+    await message.reply('**Trigger** : *Light Weight Trigger* \n**Ammunition** : *20 Round Reload* \n**Barrel** : *.45 Compact* \n**Laser** : *OWC Laser - Tactical* \n**Rear Grip** : *Stippled Grip Tape* \n \nnoob')
+    
+   if '$loadout j358' in message.content.lower() :
+    await message.reply('**Muzzle** : *MIP Light Flash Guard* \n**Barrel** : *j358 Long Barrel* \n**Trigger Action** : *Match Grade Trigger* \n**Laser** : *OWC Laser - Tactical* \n**Rear Grip** : *Granulated Grip Tape* \n \nnoob')
+    
+   if '$loadout .50gs' in message.content.lower() or '$loadout 50gs' in message.content.lower() or '$loadout deagle' in message.content.lower() :
+    await message.reply('**Muzzle** : *Monolithic Suppressor* \n**Ammunition** : * Extended Mag A* \n**Trigger Action** : *Match Grade Trigger* \n**Laser** : *OWC Laser - Tactical* \n**Rear Grip** : *Granulated Grip Tape* \n \nnoob')
+    
+   if '$loadout renetti' in message.content.lower() :
+    await message.reply('**Muzzle** : *Monolithic Suppressor* \n**Barrel** : *OWC Ranger* \n**Stock** : *Combo* \n**Trigger Action** : *Lightweight Trigger* \n**Ammunition** : *Large Extended Mag B* \n \nnoob')
 
   
   
